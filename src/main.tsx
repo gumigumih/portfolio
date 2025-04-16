4139fcfb2e5979b611282fad0990ef3097574615
gumigumih <>
2025-04-16 19:39:15 +0900

    tailwindをCDNからimportに変更

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-// ✅ Tailwind CSS を CDN で使用
-const script = document.createElement('script');
-script.src = 'https://cdn.tailwindcss.com';
-document.head.appendChild(script);
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
